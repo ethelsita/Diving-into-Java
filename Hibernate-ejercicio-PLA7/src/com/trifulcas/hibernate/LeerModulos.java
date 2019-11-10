8914fa25b03e5a28cdae4ac72241ef39b3380b0f
ethelsita <55105752+ethelsita@users.noreply.github.com>
2019-11-10 15:17:09 +0100

    Update LeerModulos.java

--- Hibernate-ejercicio-PLA7/src/com/trifulcas/hibernate/LeerModulos.java
+++ Hibernate-ejercicio-PLA7/src/com/trifulcas/hibernate/LeerModulos.java
@@ -22,7 +22,7 @@ public class LeerModulos {
 
 	public static void main(String[] args) {
 
-		// Crear la configuraci髇 cog韊ndola del xml y a馻diendo la clase Categorias
+		// Crear la configuraci贸n cog铆endola del xml y a帽adiendo las clases
 
 		Configuration configuration = new Configuration().configure("hibernate.cfg.xml")
 
@@ -36,11 +36,11 @@ public class LeerModulos {
 
 				.applySettings(configuration.getProperties());
 
-		// Crear la factor韆 de sesiones
+		// Crear la factor铆a de sesiones
 
 		SessionFactory factory = configuration.buildSessionFactory(builder.build());
 
-		// Crear la sesi髇
+		// Crear la sesi贸n
 
 		Session session = factory.getCurrentSession();
 
@@ -50,13 +50,13 @@ public class LeerModulos {
 
 
 
-			// Iniciar transacci髇
+			// Iniciar transacci贸n
 
 			session.beginTransaction();
 
 
 
-			// Leemos un producto
+			// Leemos un m贸dulo
 
 
 
@@ -68,7 +68,7 @@ public class LeerModulos {
 
 			
 
-			// commit de la transacci髇
+			// commit de la transacci贸n
 
 			session.getTransaction().commit();
 
